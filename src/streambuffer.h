42047112a8d54f5f36aa487224dae7ee51da0682
Hieu Le <trunghieule94@gmail.com>
2016-09-29 00:49:03 -0500

    Add StreamBuffer class

---
+++ src/streambuffer.h
@@ -0,0 +1,55 @@
+// Buffer class to read characters from an input stream.
+// @author Hieu Le
+// @version 09/28/2016
+
+#ifndef STREAMBUFFER_H__
+#define STREAMBUFFER_H__
+
+#include <iostream>
+#include <list>
+#include <memory>
+#include <string>
+
+#include "buffer.h"
+
+class StreamBuffer : public Buffer {
+ public:
+  // Initializes the buffer from given input stream. The associated stream
+  // remains property of the caller and shall not be destroyed or modified
+  // during the lifetime of this object.
+  explicit StreamBuffer(std::istream *stream);
+
+  // Removes and returns the next character from the stream. Returns EOF if
+  // there is no more character to read.
+  char next_char() override;
+
+  // Places a character back into the buffer.
+  void unread_char(char c) override;
+
+ private:
+  // Capacity of internal character buffer.
+  static const int MAX_BUFFER_SIZE = 1024;
+  
+  // Gets the next character and performs any necessary buffer refill.
+  char next();
+
+  // Skips the current line of characters.
+  void skip_line();
+
+  // Removes any subsequent whitespace and comment. If there is any remaining
+  // token to process, the first character of that token would be stored in the
+  // buffer front.
+  // Returns true if any removal takes place; false otherwise.
+  bool remove_space_and_comment();
+
+  // Input stream to read characters from.
+  std::istream * const stream_;
+
+  // Internal character buffer.
+  std::list<char> buffer_;
+
+  // Flags indicating if there are any remaining character to read.
+  bool exhausted;
+};
+  
+#endif
---
+++ test/streambuffer_test.cc
@@ -0,0 +1,112 @@
+// Unit tests for StreamBuffer class.
+// @author Hieu Le
+// @version 09/28/2016
+
+#include "src/streambuffer.h"
+
+#include <sstream>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+// Test if StreamBuffer generates on specified input an expected sequence of
+// characters.
+void TestNextChar(const std::string& input,
+                  const std::vector<char>& expected) {
+  std::istringstream ss(input);
+  StreamBuffer buffer(&ss);
+  for (const char c : expected) {
+    EXPECT_EQ(buffer.next_char(), c);
+  }
+}
+
+TEST(StreamBufferTest, NextCharBasic) {
+  TestNextChar("bool", {'b', 'o', 'o', 'l', EOF_MARKER});
+  TestNextChar("int a;", {'i', 'n', 't', SPACE, 'a', ';', EOF_MARKER});
+  TestNextChar("a = 3;", {'a', SPACE, '=', SPACE, '3', ';', EOF_MARKER});
+  TestNextChar("if(a )", {'i', 'f', '(', 'a', SPACE, ')', EOF_MARKER});
+  TestNextChar(";:(),=<>+-",
+               {';', ':', '(', ')', ',', '=', '<', '>', '+', '-', EOF_MARKER});
+  TestNextChar("", {EOF_MARKER});
+  TestNextChar("a", {'a', EOF_MARKER, EOF_MARKER, EOF_MARKER});
+}
+
+TEST(StreamBufferTest, NextCharWithWhitespace) {
+  TestNextChar("a b", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("a\n\nb", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("\n\n\t a", {'a', EOF_MARKER});
+  TestNextChar("a \n\t ", {'a', SPACE, EOF_MARKER});
+  TestNextChar("  \n\n\t\t  ", {EOF_MARKER});
+}
+
+TEST(StreamBufferTest, NextCharWithComments) {
+  TestNextChar("f#abc def ghi\nb", {'f', SPACE, 'b', EOF_MARKER});
+  TestNextChar("a#foo quoz bar \n#bar\nb", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("#this #is #comment\na#abcxyz", {'a', SPACE, EOF_MARKER});
+  TestNextChar("a#this\t\tis a\t\tcomment\nb", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("#this is a comment\na", {'a', EOF_MARKER});
+  TestNextChar("a#this is a comment", {'a', SPACE, EOF_MARKER});
+  TestNextChar("#this is a comment", {EOF_MARKER});
+  TestNextChar("abc #!@#$%^&*\n", {'a', 'b', 'c', SPACE, EOF_MARKER});
+  TestNextChar("a#$$$\nb", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("#$$$", {EOF_MARKER});
+}
+
+
+TEST(StreamBufferTest, NextCharWithMixingWhitespaceAndComments) {
+  TestNextChar("a #foo bar\n b", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar("#foo bar\n\n\t  a b", {'a', SPACE, 'b', EOF_MARKER});
+  TestNextChar(" a #foo bar \n \t   b #foo\n\t c#foo bar",
+               {'a', SPACE, 'b', SPACE, 'c', SPACE, EOF_MARKER});
+}
+
+TEST(StreamBufferTest, NextCharWithLongInputStream) {
+  std::string input;
+  for (int i = 0; i < 20000; ++i) {
+    input.push_back('a');
+  }
+  std::vector<char> expected(input.begin(), input.end());
+  expected.push_back(EOF_MARKER);
+  TestNextChar(input, expected);
+}
+
+TEST(StreamBufferTest, UnreadCharBasic) {
+  std::istringstream ss("a");
+  StreamBuffer buffer(&ss);
+  const char result = buffer.next_char();
+  EXPECT_EQ(result, 'a');
+  buffer.unread_char(result);
+  EXPECT_EQ(buffer.next_char(), result);
+  EXPECT_EQ(buffer.next_char(), EOF_MARKER);
+  buffer.unread_char(EOF_MARKER);
+  EXPECT_EQ(buffer.next_char(), EOF_MARKER);
+}
+
+TEST(StreamBufferDeathTest, NextCharIllegalInput) {
+  {
+    std::istringstream ss("FOO");
+    StreamBuffer buffer(&ss);
+    ASSERT_EXIT(buffer.next_char(),
+                 ::testing::ExitedWithCode(EXIT_FAILURE),
+                 "c*Invalid character: Fc*");
+  }
+  {
+    std::istringstream ss("%^&");
+    StreamBuffer buffer(&ss);
+    ASSERT_EXIT(buffer.next_char(),
+                ::testing::ExitedWithCode(EXIT_FAILURE),
+                "c*Invalid character: %c*");
+  }
+  {
+    std::istringstream ss("$");
+    StreamBuffer buffer(&ss);
+    ASSERT_EXIT(buffer.next_char(),
+                ::testing::ExitedWithCode(EXIT_FAILURE),
+                "c*Invalid character: \\$c*");
+  }
+}
+
+}  // namespace
