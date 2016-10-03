50ecf77c8b6d9da3cc83c5b74ab236fb59668129
Hieu Le <trunghieule94@gmail.com>
2016-10-03 16:04:29 -0500

    Update punctoken.h

--- src/punctoken.h
+++ src/punctoken.h
@@ -22,18 +22,18 @@ typedef enum punc_attr { PUNC_SEMI = 200,
 
 class PuncToken : public Token {
  public:
-  // Default constructor initializes attribute to PUNC_NO_ATTR.
+  // Constructs a punctuation token with PUNC_NO_ATTR as default attribute.
   PuncToken();
 
-  // Constructs a punctuation token from a given attribute.
+  // Constructs a punctuation token from a specified attribute.
   explicit PuncToken(punc_attr_type attr);
 
   ~PuncToken() override;
 
-  // Returns the attribute of this token.
+  // Returns the attribute of this punctuation token.
   punc_attr_type get_attribute() const;
 
-  // Sets the attribute of this token.
+  // Sets the attribute of this punctuation token to a specified value.
   void set_attribute(punc_attr_type attr);
 
   // Debug string will be of the form TOKEN_PUNC:<punc_attr_type>.
