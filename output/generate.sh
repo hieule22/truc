61c5bdfea579f4fc82628722a0e255b18248c31c
Hieu Le <trunghieule94@gmail.com>
2016-10-03 16:46:49 -0500

    Update bash script

--- output/generate.sh
+++ output/generate.sh
@@ -5,15 +5,25 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET="src"
 SRC="$DIR/../$TARGET"
-
 PACKAGE_NAME="le_h_init.tar"
 
-# Change directory to source directory.
-cd $DIR/..
+# Command line args.
+ARG=$1
+
+if [ $# -eq 1 ] && [ $ARG == "bazel" ]; then
+   # Build all targets.
+   cd $DIR/../src/
+   bazel build :all
+
+   # Run all tests
+   cd $DIR/../test/
+   bazel test :all
+fi
 
 # Package all relevant files.
-tar c --exclude="BUILD" --exclude="*.o" --exclude="test_scanner" \
-    --exclude-backups -vf $PACKAGE_NAME $TARGET
+cd $DIR/..
+tar c --exclude="*~" --exclude="*.o" --exclude="test_scanner" \
+    --exclude="BUILD" -vf $PACKAGE_NAME $TARGET
 
 # Move created package to output.
 mv $PACKAGE_NAME $DIR
