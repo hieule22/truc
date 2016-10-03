fe3bd2887064614d043b466c439054a19c390577
Hieu Le <trunghieule94@gmail.com>
2016-10-03 16:05:39 -0500

    Update reloptoken.h

--- src/reloptoken.h
+++ src/reloptoken.h
@@ -1,4 +1,4 @@
-// Token class for relation operators from TruPL.
+// Token class for relational operators from TruPL.
 // @author Hieu Le
 // @version 09/28/2016
 
@@ -22,18 +22,18 @@ typedef enum relop_attr { RELOP_EQ = 300,
 
 class RelopToken : public Token {
  public:
-  // Defaults constructor initializes attribute to RELOP_NO_ATTR.
+  // Constructs a relop token with RELOP_NO_ATTR as default attribute.
   RelopToken();
 
-  // Constructs a token from a specified attribute.
+  // Constructs a relop token from a specified attribute.
   explicit RelopToken(relop_attr_type attr);
 
   ~RelopToken() override;
 
-  // Returns the attribute of this token.
+  // Returns the attribute of this relop token.
   relop_attr_type get_attribute() const;
 
-  // Sets the attribute of this token to given value.
+  // Sets the attribute of this relop token to a specified value.
   void set_attribute(relop_attr_type attr);
 
   // Debug string will be of the form TOKEN_RELOP:<relop_attr_type>.
